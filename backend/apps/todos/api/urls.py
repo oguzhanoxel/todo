0c59e0f4d5e105c38b5e077338fe2792758fb1a4
oguzhanoxel <oguzhanoksel@hotmail.com>
2021-04-22 19:57:13 +0300

    added api in apps.todos, added CORS in config.settings

---
+++ backend/apps/todos/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import TodoList, TodoDetail
+
+
+urlpatterns = [
+    path('', TodoList.as_view()),
+    path('<int:pk>', TodoDetail.as_view()),
+]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'apps.todos',
 ]
 
@@ -119,3 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
+# CORS
+
+CORS_ORIGIN_WHITELIST = [
+    'https://localhost:3000'
+]
