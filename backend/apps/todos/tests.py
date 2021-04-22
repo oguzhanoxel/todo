a13f901880e85eb411ea4ecdc51b7fa0782d6c9d
oguzhanoxel <oguzhanoksel@hotmail.com>
2021-04-22 19:27:04 +0300

    added model, admin, test in apps.todos

--- backend/apps/todos/tests.py
+++ backend/apps/todos/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='Test Todo')
+        Todo.objects.create(description='I am a test description')
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEquals(expected_object_name, 'Test Todo')
+
+    def test_description_content(self):
+        todo = Todo.objects.get(id=2)
+        expected_object_name = f'{todo.description}'
+        self.assertEquals(expected_object_name, 'I am a test description')
